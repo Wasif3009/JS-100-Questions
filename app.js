a4ea94bddfadbe6f6d0d122bfc0f8d3658f3524f
Wasif3009 <wasifshaikh1233@gmail.com>
2025-04-24 17:05:15 +0530

    DAY 2

--- app.js
+++ app.js
@@ -1,35 +1,95 @@
 //JS 100 QUESTIONS
 console.log("JS 100 QUESTIONS");
 ////////////////////////////////////////////////////////////////////////////////
+//DAY 1
 //1.JavaScript Program To Print Hello World
 //METHOD 1
-console.log("HELLO WORLD BY METHOD 1");
+// console.log("HELLO WORLD BY METHOD 1");
 //METHOD 2
-function printHello(str) {
-  return str;
-}
-console.log(printHello("HELLO WORLD BY METHOD 2"));
+// function printHello(str) {
+//   return str;
+// }
+// console.log(printHello("HELLO WORLD BY METHOD 2"));
 ////////////////////////////////////////////////////////////////////////////////
 //2.JavaScript Program to Add Two Numbers
 //METHOD 1
-let a = 5;
-let b = 5;
-let c = a + b;
-console.log(`THE ADDITION IS ${c} BY METHOD 1`);
+// let a = 5;
+// let b = 5;
+// let c = a + b;
+// console.log(`THE ADDITION IS ${c} BY METHOD 1`);
 //METHOD 2
-function addTwoNums(x, y) {
-  return `THE ADDITION IS ${x + y} BY METHOD 2`;
-}
-console.log(addTwoNums(10, 5));
+// function addTwoNums(x, y) {
+//   return `THE ADDITION IS ${x + y} BY METHOD 2`;
+// }
+// console.log(addTwoNums(10, 5));
 ////////////////////////////////////////////////////////////////////////////////
 //3.JavaScript Program to Find the Square Root
 //METHOD 1
-let input = 5;
-let sqrRoot = input * input;
-console.log(`THE SQUARE ROOT IS ${sqrRoot} BY METHOD 1`);
+// let input = 5;
+// let sqrRoot = input * input;
+// console.log(`THE SQUARE ROOT IS ${sqrRoot} BY METHOD 1`);
 //METHOD 2
-function sqrRootFunc(num) {
-  return `THE SQUARE ROOT IS ${num * num} BY METHOD 2`;
-}
-console.log(sqrRootFunc(10));
+// function sqrRootFunc(num) {
+//   return `THE SQUARE ROOT IS ${num * num} BY METHOD 2`;
+// }
+//console.log(sqrRootFunc(10));
+////////////////////////////////////////////////////////////////////////////////
+//DAY 2
+//4.JavaScript Program to Calculate the Area of a Triangle
+//METHOD 1
+// let base = 5;
+// let height = 10;
+// let area = (base * height) / 2;
+// console.log(`AREA OF TRIANGLE IS ${area} BY METHOD 1`);
+// //METHOD 2
+// function areaOfTriangle(b, h) {
+//   let answer = (b * h) / 2;
+//   return `AREA OF TRIANGLE IS ${answer} BY METHOD 2`;
+// }
+// console.log(areaOfTriangle(10, 10));
+////////////////////////////////////////////////////////////////////////////////
+//5.JavaScript Program to Swap Two Variables
+//METHOD 1
+
+// let a = 2;
+// let b = 5;
+// console.log(`VALUE OF A BEFORE SWAPPING ${a}`);
+// console.log(`VALUE OF B BEFORE SWAPPING ${b}`);
+// let temp = a;//temp = 2
+// a = b;// a = 5
+// b = temp;//b = 2
+// console.log(`VALUE OF A AFTER SWAPPING ${a}`);
+// console.log(`VALUE OF B AFTER SWAPPING ${b}`);
+
+//METHOD 2
+// function swapping(x, y) {
+//   let a = x;
+//   let b = y;
+//   console.log(`VALUE OF X BEFORE SWAPPING ${a}`);
+//   console.log(`VALUE OF Y BEFORE SWAPPING ${b}`);
+//   let temp = a;
+//   let temp2 = b;
+//   a = temp2;
+//   b = temp;
+//   console.log(`VALUE OF X AFTER SWAPPING ${a}`);
+//   console.log(`VALUE OF Y AFTER SWAPPING ${b}`);
+// }
+// swapping(10, 20);
+////////////////////////////////////////////////////////////////////////////////
+//6.JavaScript Program to Solve Quadratic Equation
+//METHOD 1
+// let a = 2;
+// let b = 4;
+// let c = 1;
+// let answer = -b + -(Math.sqrt(b * b - 4 * (a * c)) / 2) * a; //THIS IS FORMULA BASE QUESTION SO IT CAN VARY
+// console.log(answer);
+// // //METHOD 2
+// function quadratic(num1, num2, num3) {
+//   let x = num1;
+//   let y = num2;
+//   let z = num3;
+//   let finalAnswer = -y + -(Math.sqrt(y * y - 4 * (x * z)) / 2) * x;
+//   return finalAnswer;
+// }
+// console.log(quadratic(2, 4, 1));
 ////////////////////////////////////////////////////////////////////////////////
