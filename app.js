103817647112541bf01f61ee4c8f5089280553cd
Wasif3009 <wasifshaikh1233@gmail.com>
2025-09-12 16:56:48 +0530

    commit

--- app.js
+++ app.js
@@ -1194,3 +1194,18 @@ console.log("JS 100 QUESTIONS");
 // }
 // console.log(fibonacci(10));
 ////////////////////////////////////////////////////////////////////////////////
+// 81  . WRITE A FUNCTION TO CHECK IF A STRING IS PALINDROME
+
+// function palindrome(str) {
+//   let revStr = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     revStr += str[i];
+//   }
+//   if (str === revStr) {
+//     return "Palindrome";
+//   }
+//   return "Not";
+// }
+// console.log(palindrome("madam")); // Palindrome
+////////////////////////////////////////////////////////////////////////////////
